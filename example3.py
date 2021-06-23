3001689098f5e8075cd8988a5232505665d80ae9
YutoMoriyasu <y.moriyasu@icloud.com>
2021-06-23 19:12:25 +0900

    first-commit

--- example3.py
+++ example3.py
@@ -1,6 +1,6 @@
 from google.colab import files
 
-f = open('data3.csv','w')
+f = open('data4.csv','w')
 
 list1=[[ [] for i in range(101)] for j in range(1001)]
 for i in range(101):
@@ -10,13 +10,15 @@ list1[0][50]=500.0
 dx=1.0
 dt=0.1
 alpha=1.0
+u=0.2
 c1=(alpha*dt)/(dx*dx)
+c2=(u*dt)/(2*dx)
 
 for istep in range(0,1000):
         if istep%100==0:
                 print('step = '+str(istep))
         for i in range(1,100):
-                list1[istep+1][i]=list1[istep][i]+c1*(list1[istep][i+1]-2.0*list1[istep][i]+list1[istep][i-1])
+                list1[istep+1][i] = list1[istep][i] - c2 * (list1[istep][i+1] - list1[istep][i-1]) + c1*(list1[istep][i+1] - list1[istep][i] * 2.0 + list1[istep][i-1])
         list1[istep+1][0]=300.0
         list1[istep+1][100]=300.0
 
@@ -29,4 +31,4 @@ for i in range(101):
 
 f.close()
 
-files.download("data3.csv")
+files.download("data4.csv")
