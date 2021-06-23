88663b549820c22a9992b2f7dac8fe0828c02493
YutoMoriyasu <77400028+YutoMoriyasu@users.noreply.github.com>
2021-06-23 19:07:02 +0900

    Create example3.py

---
+++ example3.py
@@ -0,0 +1,32 @@
+from google.colab import files
+
+f = open('data3.csv','w')
+
+list1=[[ [] for i in range(101)] for j in range(1001)]
+for i in range(101):
+        list1[0][i]=300.0
+list1[0][50]=500.0
+
+dx=1.0
+dt=0.1
+alpha=1.0
+c1=(alpha*dt)/(dx*dx)
+
+for istep in range(0,1000):
+        if istep%100==0:
+                print('step = '+str(istep))
+        for i in range(1,100):
+                list1[istep+1][i]=list1[istep][i]+c1*(list1[istep][i+1]-2.0*list1[istep][i]+list1[istep][i-1])
+        list1[istep+1][0]=300.0
+        list1[istep+1][100]=300.0
+
+for i in range(101):
+        f.write(str(i)+',')
+        for istep in range(1,1001):
+                if istep%100==0:
+                        f.write(str(list1[istep][i])+',')
+        f.write('\n')
+
+f.close()
+
+files.download("data3.csv")
